57b1a1605c64e218f11b77cc27483f0059ac181c
Sultanbek04 <86606723+Sultanbek04@users.noreply.github.com>
2022-10-11 17:33:40 +0100

    Add files via upload

---
+++ AllowPrint.java
@@ -0,0 +1,12 @@
+package com.google.lesson_18;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AllowPrint {
+    boolean key1();
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.google.lesson_18;
+
+public class Main {
+
+    @AllowPrint(key1 = true)
+    public int n = 0;
+
+    public static void main(String[] args) {
+
+    }
+}
